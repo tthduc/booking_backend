27cc675a17daa68fe39332f46793fe822b94518b
pmchauuu <chau.pham@terralogic.com>
2024-02-29 23:23:23 +0700

    test: unit test for rate

--- db/sqlc/unit_test/rate_test.go
+++ db/sqlc/unit_test/rate_test.go
@@ -4,8 +4,10 @@ import (
 	db "booking-backed/db/sqlc"
 	"booking-backed/util"
 	"context"
-	"github.com/stretchr/testify/require"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
 )
 
 func createRandomRate(t *testing.T) db.Rate {
@@ -43,4 +45,22 @@ func createRandomRate(t *testing.T) db.Rate {
 
 func TestCreateRate(t *testing.T) {
 	createRandomRate(t)
+}
+
+func TestUpdateRate(t *testing.T) {
+	rate1 := createRandomRate(t)
+	arg := db.UpdateRateParams{
+		HotelID: rate1.HotelID,
+		RoomID:  rate1.RoomID,
+		UserID:  rate1.UserID,
+		Rate:    util.RandomInt(0, 5),
+	}
+	rate2, err := testQueries.UpdateRate(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, rate2)
+	require.Equal(t, rate1.HotelID, rate2.HotelID)
+	require.Equal(t, rate1.RoomID, rate2.RoomID)
+	require.Equal(t, rate1.UserID, rate2.UserID)
+	require.Equal(t, arg.Rate, rate2.Rate)
+	require.WithinDuration(t, rate1.CreatedAt, rate2.CreatedAt, time.Second)
 }
