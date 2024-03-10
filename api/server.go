176fd656237d91d7da54ba71e3281a5c0934bb1f
hanettechnology <thanhduc736@gmail.com>
2024-03-10 19:07:04 +0700

    feat: create api for hotel

---
+++ api/server.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	db "booking-backed/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	// TODO: add routes to router
+	router.POST("/hotel", server.createHotel)
+	router.GET("/hotel:id", server.getHotel)
+	router.POST("/hotels", server.listHotel)
+
+	server.router = router
+	return server
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
